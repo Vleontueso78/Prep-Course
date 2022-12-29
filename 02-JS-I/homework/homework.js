6b240897f31fa37c6a538cc463c4b9548f0ea668
Vleontueso78 <vleontueso78@gmail.com>
2022-12-29 17:36:44 -0300

    3 Carpetas revisadas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola"; 
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 15;
+const nuevoNum = 54;
 
 // Crea una variable booleana:
 const nuevoBool = true;
