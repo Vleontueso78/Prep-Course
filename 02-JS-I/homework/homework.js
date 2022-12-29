81d90fa01b47773ceddd8d6eab17ecea33b9cda6
Vleontueso78 <vleontueso78@gmail.com>
2022-12-29 17:29:21 -0300

    Cristian es mi amigo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola"; 
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 54;
+const nuevoNum = 15;
 
 // Crea una variable booleana:
 const nuevoBool = true;
