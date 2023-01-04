c57d87d7a01f52611e2256cbaa80c19e3738f202
Victor Tueso <vleontueso78@gmail.com>
2023-01-04 19:06:18 -0300

    Cuarto cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola'; 
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 33;
+const nuevoNum = 5657;
 
 // Crea una variable booleana:
 const nuevoBool = null;
