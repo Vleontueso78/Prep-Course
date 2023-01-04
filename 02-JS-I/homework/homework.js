0ff607a2f2a8d391816c87d8ee4532f079158121
Victor Tueso <vleontueso78@gmail.com>
2023-01-04 19:31:38 -0300

    Sexto cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -208,8 +208,8 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if(letra.length > 1) return "Dato incorrecto"
-  if(letra !== "a" || letra !== "e" || letra !== "i" || letra !== "o" || letra !== "u") return "Dato incorrecto"
-  return "Es vocal"
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") return "Es vocal"
+  return "Dato incorrecto"
 }
 
 
